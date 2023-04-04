8afe9624397c9b860c0c3ba4be57830f893562a5
MarcdeMunnik <93315020+MarcdeMunnik@users.noreply.github.com>
2023-04-04 16:02:28 +0200

    search function en search form toegevoegd

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,14 +11,34 @@
                     </div>
 
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+
+                        <form method="GET" action="{{ route('home') }}">
+                            <div class="form-group">
+                                <input type="text" name="search" class="form-control" placeholder="Zoek voor bands..." autocomplete="off    ">
+                            </div>
+                            <button type="submit" class="btn btn-primary">Zoek</button>
+                        </form>
+
+                        @if ($bands->isNotEmpty())
+                            <div class="row">
+                                @foreach ($bands as $band)
+                                    <div class="col-md-3">
+                                        <h3>{{ $band->name }}</h3>
+                                    </div>
+                                @endforeach
+                            </div>
+                        @else
+                            <p>Geen bands gevonden.</p>
+                        @endif
+
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-@endsection
+    @endsection
