ac79ebb84be390cd2d1845d33d88b69a08f590c7
quentinwehkamp <quentinwehkamp@gmail.com>
2023-05-15 11:38:43 +0200

    begonnen met front end van band show

--- resources/views/band/show.blade.php
+++ resources/views/band/show.blade.php
@@ -20,72 +20,73 @@ if (isset($id)) {
 
 @section('content')
     <div class="container" style="background-color: {{ $band->bgcolour }}; color: {{ $band->txtcolour }}">
-        @if (session('success'))
-            <div class="alert alert-success">
-                {{ session('success') }}
-            </div>
-        @endif
         <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="epk">
-                    <div class="row my-5">
+            <div class="col-md-9">
+                <div class="card">
+                    <div class="card-header">
                         @if (isset($id) && in_array($id, (array) $admin))
-                            <div id="edit">
-                                <a href="{{ route('band.edit', $band->id)}}">Edit</a>
+                            <div>
+                                <a class="button" href="{{ route('band.edit', $band->id) }}">Edit</a>
                             </div>
                         @endif
-
-                        <div class="col-md-5">
-                            <img class="object-fit border rounded bandImg" src={{ asset($band->imgid) }} alt="">
-                        </div>
-                        <div class="col-md">
-                            <div class="col-md">
-                                <h1>{{ $band->name }}</h1>
-                            </div>
-                            <div class="col-md">
-                                <h3>{{ $band->created_at }}</h3>
-                            </div>
-                            <div class="col-md">
-                                <p>{{ $band->bio }}</p>
-                            </div>
-                        </div>
                     </div>
-                    <div class="row">
-                        <div class="col-md">
-                            <iframe width="280" height="157" src="https://www.youtube.com/embed/{{ $newyt0[1] }}"
-                                title="YouTube video player" frameborder="0"
-                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                                allowfullscreen>
-                            </iframe>
+                    <div class="epk">
+                        <div class="row my-5">
+
+                            <div class="col-md-5">
+                                <img class="object-fit border rounded bandImg" src={{ asset($band->imgid) }} alt="">
+                            </div>
+                            <div class="col-md">
+                                <div class="col-md">
+                                    <h1>{{ $band->name }}</h1>
+                                </div>
+                                <div class="col-md">
+                                    <p>{{ $band->desc }}</p>
+                                </div>
+                                <div class="col-md">
+                                    <p>{{ $band->bio }}</p>
+                                </div>
+                            </div>
                         </div>
-                        <div class="col-md">
-                            <iframe width="280" height="157" src="https://www.youtube.com/embed/{{ $newyt1[1] }}"
-                                title="YouTube video player" frameborder="0"
-                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                                allowfullscreen>
-                            </iframe>
-                        </div>
-                        <div class="col-md">
-                            <iframe width="280" height="157" src="https://www.youtube.com/embed/{{ $newyt2[1] }}"
-                                title="YouTube video player" frameborder="0"
-                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                                allowfullscreen>
-                            </iframe>
-                        </div>
-                        <div class="col-md">
-                            @if (isset($newyt3))
+                        <div class="row">
+                            <div class="col-md">
                                 <iframe width="280" height="157"
-                                    src="https://www.youtube.com/embed/{{ $newyt3[1] }}" title="YouTube video player"
+                                    src="https://www.youtube.com/embed/{{ $newyt0[1] }}" title="YouTube video player"
                                     frameborder="0"
                                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                                     allowfullscreen>
                                 </iframe>
-                            @endif
+                            </div>
+                            <div class="col-md">
+                                <iframe width="280" height="157"
+                                    src="https://www.youtube.com/embed/{{ $newyt1[1] }}" title="YouTube video player"
+                                    frameborder="0"
+                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                    allowfullscreen>
+                                </iframe>
+                            </div>
+                            <div class="col-md">
+                                <iframe width="280" height="157"
+                                    src="https://www.youtube.com/embed/{{ $newyt2[1] }}" title="YouTube video player"
+                                    frameborder="0"
+                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                    allowfullscreen>
+                                </iframe>
+                            </div>
+                            <div class="col-md">
+                                @if (isset($newyt3))
+                                    <iframe width="280" height="157"
+                                        src="https://www.youtube.com/embed/{{ $newyt3[1] }}"
+                                        title="YouTube video player" frameborder="0"
+                                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                        allowfullscreen>
+                                    </iframe>
+                                @endif
+                            </div>
                         </div>
                     </div>
-                </div>
 
+                </div>
             </div>
         </div>
-    </div>
-@endsection
+    @endsection
