a1a7978d740481e65fface38a440a12cd510bd6f
quentinwehkamp <quentinwehkamp@gmail.com>
2023-05-30 11:40:04 +0200

    front end voor band pagina gemaakt en probeer nu destroy te gebruiken

--- resources/views/band/show.blade.php
+++ resources/views/band/show.blade.php
@@ -21,61 +21,60 @@ if (isset($id)) {
 @section('content')
     <div class="container" style="background-color: {{ $band->bgcolour }}; color: {{ $band->txtcolour }}">
         <div class="row justify-content-center">
-            <div class="col-md-9">
+            <div class="col-md-10">
                 <div class="card">
-                    <div class="card-header">
-                        @if (isset($id) && in_array($id, (array) $admin))
-                            <div>
-                                <a class="button" href="{{ route('band.edit', $band->id) }}">Edit</a>
-                            </div>
-                        @endif
-                    </div>
-                    <div class="epk">
-                        <div class="row my-5">
-
-                            <div class="col-md-5">
-                                <img class="object-fit border rounded bandImg" src={{ asset($band->imgid) }} alt="">
-                            </div>
-                            <div class="col-md">
-                                <div class="col-md">
-                                    <h1>{{ $band->name }}</h1>
-                                </div>
-                                <div class="col-md">
-                                    <p>{{ $band->desc }}</p>
-                                </div>
-                                <div class="col-md">
-                                    <p>{{ $band->bio }}</p>
-                                </div>
+                    @if (isset($id) && in_array($id, (array) $admin))
+                        <div class="card-header">
+                            <div class="text-end">
+                                <a class="btn btn-outline-dark me-1" href="{{ route('band.edit', $band->id) }}">Edit</a>
+                                <a class="btn btn-danger btn-outline-dark" href="{{ route('band.destroy', $band->id) }}">Delete</a>
                             </div>
                         </div>
-                        <div class="row">
-                            <div class="col-md">
-                                <iframe width="280" height="157"
+                    @endif
+                    <div class="epk d-flex flex-row flex-wrap overflow-auto position-relative py-3 px-3">
+
+                        <div class="col-1 pt-1 me-5">
+                            <img class="rounded bandImg" src={{ asset($band->imgid) }} alt="">
+                        </div>
+                        <div class="d-flex flex-column pt-3 col ms-5 text-wrap">
+                            <div class="">
+                                <h1>{{ $band->name }}</h1>
+                            </div>
+                            <div class="">
+                                <p>{{ $band->desc }}</p>
+                            </div>
+                            <div class="">
+                                <p>{{ $band->bio }}</p>
+                            </div>
+                        </div>
+                        <div class="d-flex flex-column pt-1">
+                            <div class="">
+                                <iframe class="rounded" width="280" height="157"
                                     src="https://www.youtube.com/embed/{{ $newyt0[1] }}" title="YouTube video player"
                                     frameborder="0"
-                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                    allow=""
                                     allowfullscreen>
                                 </iframe>
                             </div>
-                            <div class="col-md">
-                                <iframe width="280" height="157"
+                            <div class="">
+                                <iframe class="rounded" width="280" height="157"
                                     src="https://www.youtube.com/embed/{{ $newyt1[1] }}" title="YouTube video player"
                                     frameborder="0"
                                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                                     allowfullscreen>
                                 </iframe>
                             </div>
-                            <div class="col-md">
-                                <iframe width="280" height="157"
+                            <div class="">
+                                <iframe class="rounded" width="280" height="157"
                                     src="https://www.youtube.com/embed/{{ $newyt2[1] }}" title="YouTube video player"
                                     frameborder="0"
                                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                                     allowfullscreen>
                                 </iframe>
                             </div>
-                            <div class="col-md">
+                            <div class="">
                                 @if (isset($newyt3))
-                                    <iframe width="280" height="157"
+                                    <iframe class="rounded" width="280" height="157"
                                         src="https://www.youtube.com/embed/{{ $newyt3[1] }}"
                                         title="YouTube video player" frameborder="0"
                                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
